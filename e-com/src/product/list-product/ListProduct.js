f15929b3bf5cc7ee3ebb1c0e68b60d3637360b6f
Dheeraj <dheeraj.s.roy@gmail.com>
2022-01-21 12:31:49 +0530

    form-submit

--- e-com/src/product/list-product/ListProduct.js
+++ e-com/src/product/list-product/ListProduct.js
@@ -2,9 +2,12 @@
 import './ListProduct.css';
 import NavBar from '../../shared/nav-bar/NavBar';
 import { useState, useEffect } from "react";
+import {useNavigate} from "react-router-dom";
 
 
 function ListProduct() {
+
+    const navigate = useNavigate();
 
     // Abstraction/Encapsulation
     const [products, setProducts] = useState([]);
@@ -18,10 +21,16 @@ function ListProduct() {
                 })
     }, []);
 
+    
+    function goToForm(){
+        navigate("/product/0")
+    }
+    
     return (
         <div>
             <NavBar></NavBar>
             <div className='container'>
+                <button type='button' onClick={goToForm} className='btn btn-primary'>New Product</button>
                 <table className="table table-primary">
                     <thead>
                         <tr>
--- e-com/src/product/product-form/ProductForm.js
+++ e-com/src/product/product-form/ProductForm.js
@@ -5,33 +5,44 @@ import './ProductForm.css'
 function ProductForm() {
 
 
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log(event.target.elements);
+        const name=event.target.elements[0];
+        const author=event.target.elements[1];
+        const imgSrc=event.target.elements[2];
+        const details=event.target.elements[3];
+        const price=event.target.elements[4];
+        
+    }
+
     return (
         <div>
             <NavBar></NavBar>
             <div className="container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Name</label>
+                        <label className="form-label">Name</label>
                         <input type="text" className="form-control" name="inputName" placeholder="Name" />
                     </div>
 
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Author</label>
+                        <label className="form-label">Author</label>
                         <input type="text" className="form-control" name="inputAuthor" />
                     </div>
 
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">ImgSrc</label>
+                        <label className="form-label">ImgSrc</label>
                         <input type="text" className="form-control" name="inputImgSrc" />
                     </div>
 
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Details</label>
+                        <label className="form-label">Details</label>
                         <input type="text" className="form-control" name="inputDetails" />
                     </div>
 
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Price</label>
+                        <label className="form-label">Price</label>
                         <input type="number" className="form-control" name="inputPrice" />
                     </div>
 
